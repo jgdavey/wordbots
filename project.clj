5dec82158927b9a0acd4e46a6506c9ab03f8172b
Joshua Davey <josh@joshuadavey.com>
2015-05-22 10:41:20 -0500

    Use released version of vecset

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.generators "0.1.2"]
+                 [com.joshuadavey/vecset "0.2.0"]
                  [compojure  "1.3.1"]
                  [ring/ring-json "0.3.1"]]
   :plugins [[lein-ring "0.8.13" :exclusions [org.clojure/clojure]]]
