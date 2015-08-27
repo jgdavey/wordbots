52be553faa670d500f27fe088f58e7603959f0ea
Joshua Davey <josh@joshuadavey.com>
2015-08-27 12:18:28 -0400

    Bump clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.generators "0.1.2"]
                  [com.joshuadavey/vecset "0.2.0"]
                  [compojure  "1.3.1"]
