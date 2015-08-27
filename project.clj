5706aedc65e6d9517e68751699118613338dc854
Joshua Davey <josh@joshuadavey.com>
2015-08-27 12:31:00 -0400

    Bump some dependencies

--- project.clj
+++ project.clj
@@ -6,9 +6,11 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.generators "0.1.2"]
                  [com.joshuadavey/vecset "0.2.0"]
-                 [compojure  "1.3.1"]
-                 [ring/ring-json "0.3.1"]]
-  :plugins [[lein-ring "0.8.13" :exclusions [org.clojure/clojure]]]
+                 [compojure "1.4.0"]
+                 [instaparse "1.4.1" :exclusions [org.clojure/clojure]]
+                 [clj-http "2.0.0"]
+                 [ring/ring-json "0.4.0"]]
+  :plugins [[lein-ring "0.9.6" :exclusions [org.clojure/clojure]]]
   :ring {:init wordbots.handler/init
          :handler wordbots.handler/app}
   :profiles {:uberjar {:aot :all}}
