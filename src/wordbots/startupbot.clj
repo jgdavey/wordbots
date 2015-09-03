f45b88b3ce296aad823de916c6359950f63f80a2
Joshua Davey <josh@joshuadavey.com>
2015-09-02 21:23:12 -0400

    Take startupbot "in-house"

--- src/wordbots/startupbot.clj
+++ src/wordbots/startupbot.clj
@@ -1,38 +1,42 @@
 (ns wordbots.startupbot
-  (:require [wordbots.protocols :as p]
-            [clj-http.client :as client]))
+  (:require [clojure.java.io :as io]
+            [wordbots.protocols :as p]
+            [wordbots.util :refer [lines paragraphs]]))
 
-(def ^:static api "http://itsthisforthat.com/api.php?json")
+(def ^:static responses ["My new startup is like %s for %s."
+                         "So, basically, it's like %s for %s."
+                         "Have you heard of %s? It's like that, but for %s."
+                         "Check this out. It's like %s for %s."
+                         "I really want to do something like %s, but for %s."
+                         "We're trying to fill the same space as %s, only for %s instead."
+                         "It's like %s for %s."
+                         "So it's like %s for %s."
+                         "The idea is simple. It's %s for %s."])
 
-(def ^:static responses ["My new startup is like a %s for %s."
-                         "So, basically, it's like a %s for %s."
-                         "Have you heard of a %s? It's like that, but for %s."
-                         "Check this out. It's like a %s for %s."
-                         "I really want to do something like a %s, but for %s."
-                         "We're trying to fill the same space as a %s, only for %s instead."
-                         "It's like a %s for %s."
-                         "So it's like a %s for %s."
-                         "The idea is simple. It's a %s for %s."])
+(defn index [a]
+  (let [file->lines #(->> (io/resource %) slurp lines)]
+    (swap! a assoc :this (file->lines "startup/this.txt"))
+    (swap! a assoc :that (file->lines "startup/that.txt"))
+    :ok))
 
-(defn fetch []
-  (some-> (client/get api {:as :json})
-          :body))
+(defn generate* [index]
+  (format (rand-nth responses)
+          (rand-nth (:this index))
+          (rand-nth (:that index))))
 
-(defn generate* []
-  (let [resp (fetch)]
-    (format (rand-nth responses) (:this resp) (:that resp))))
-
-(defrecord Startupbot []
-  p/Bot
-  (init [_])
-  (generate [_ req] (generate*)))
-
-(def sb (->Startupbot))
-(def bot (constantly sb))
-
+(defn bot []
+  (let [idx (atom {:this [] :that []})]
+    (reify
+      p/Bot
+      (init [_]
+        (index idx))
+      (generate [_ _]
+        (generate* @idx)))))
 
 (comment
 
+(def sb (bot))
+(p/init sb)
 (p/generate sb {})
 
 )
