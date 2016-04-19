a4090b3cf7bbcc4fa230adfc2afb6a0400af55a2
Joshua Davey <josh@joshuadavey.com>
2016-04-19 11:52:32 -0400

    Bump Clojure, deps

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.generators "0.1.2"]
                  [com.joshuadavey/vecset "0.2.0"]
-                 [compojure "1.4.0"]
+                 [compojure "1.5.0"]
                  [instaparse "1.4.1" :exclusions [org.clojure/clojure]]
-                 [image-resizer  "0.1.7"]
-                 [clj-http "2.0.0"]
+                 [image-resizer  "0.1.9"]
+                 [clj-http "2.1.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.9.6" :exclusions [org.clojure/clojure]]]
