c6cab0f377807f7b3f4d7a97ed1ac645a0acba4c
Joshua Davey <josh@suiteness.com>
2018-08-14 11:30:05 -0400

    Add plugable markov path encoders

--- src/wordbots/markov.clj
+++ src/wordbots/markov.clj
@@ -4,13 +4,29 @@
             [clojure.data.generators :as gen]
             [clojure.java.io :as io]))
 
-(def tuple-vector vector)
+(defprotocol PathEncoder
+  (-encode-prefix-path [_ prefixes]))
 
-(defrecord MarkovIndex [tuple-size forward-index backward-index entries])
+(defn default-encoder []
+  (reify PathEncoder
+    (-encode-prefix-path [_ prefixes] [(vec prefixes)])))
+
+(defn nested-encoder []
+  (reify PathEncoder
+    (-encode-prefix-path [_ prefixes] (vec prefixes))))
+
+(defn string-encoder [separator]
+  (reify PathEncoder
+    (-encode-prefix-path [_ prefixes] [(str/join separator prefixes)])))
+
+(defrecord MarkovIndex [tuple-size forward-index backward-index entries encoder])
+
+(defn encode-prefix [{:keys [encoder] :as idx} prefix]
+  (-encode-prefix-path encoder prefix))
 
 (defn markov-index-factory
   ([] (markov-index-factory 2))
-  ([tuple-size] (->MarkovIndex tuple-size {} {} {})))
+  ([tuple-size] (->MarkovIndex tuple-size {} {} {} (nested-encoder))))
 
 (defn update-in!
   [m [k & ks] f & args]
@@ -35,22 +51,17 @@
 (defn add-markers [n tokens]
   (concat (repeat n ::boundary) tokens [::boundary]))
 
-(defn prefix-pair [coll]
-  (conj [(apply tuple-vector (butlast coll))] (last coll)))
-
-(defn tokens->prefix-pairs [tuple-size tokens]
-  (->> tokens
-       (add-markers tuple-size)
-       (partition (+ 1 tuple-size) 1)
-       (map prefix-pair)))
-
-(defn index-tokens [m tuple-size tokens]
-  (let [pairs (tokens->prefix-pairs tuple-size tokens)]
+(defn index-tokens [{:keys [tuple-size] :as idx} dir tokens]
+  (let [m (get idx dir)
+        pairs (->> tokens
+                   (add-markers tuple-size)
+                   (partition (+ 1 tuple-size) 1)
+                   (map #(conj (encode-prefix idx (butlast %)) (last %))))]
     (persistent! (reduce index-path (transient m) pairs))))
 
-(defn index-entries [m tuple-size tokens]
-  (let [pairs (mapv (juxt first (partial apply tuple-vector)) (partition tuple-size 1 tokens))]
-    (persistent! (reduce index-path (transient m) pairs))))
+(defn index-entries [{:keys [entries tuple-size]} tokens]
+  (let [pairs (mapv (juxt first vec) (partition tuple-size 1 tokens))]
+    (persistent! (reduce index-path (transient entries) pairs))))
 
 (defn index
   "Index String data into index MarkovIndex idx, optionally using tokenizer"
@@ -58,10 +69,10 @@
    (index idx data words))
   ([idx ^String data tokenizer]
    (let [tokens (tokenizer data)
-         forward (index-tokens (:forward-index idx) (:tuple-size idx) tokens)
-         backward (index-tokens (:backward-index idx) (:tuple-size idx) (reverse tokens))
-         entries (index-entries (:entries idx) (:tuple-size idx) tokens)]
-     (->MarkovIndex (:tuple-size idx) forward backward entries))))
+         forward (index-tokens idx :forward-index tokens)
+         backward (index-tokens idx :backward-index (reverse tokens))
+         entries (index-entries idx tokens)]
+     (->MarkovIndex (:tuple-size idx) forward backward entries (:encoder idx)))))
 
 (defn ensure-trailing-punctuation [^String string]
   (let [c (last string)]
@@ -85,29 +96,30 @@
   (/ (reduce + 0 (map :score nodes)) (count nodes)))
 
 (defn generate-sequence
-  "Using (forward or backward) index m, tuple-size t, starting at key k, generate sequence of tuples.
-  Stops at a ::boundary, or after max tuples, whatever comes first."
-  [m t k max-words]
-  (loop [acc (if (coll? k) k [k])]
-    (let [token (apply tuple-vector (subvec acc (max (- (count acc) t) 0)))
-          d (get m token)
-          nextword (when (pos? (count d))
-                     (gen/weighted d))]
-      (if (and d nextword (< (count acc) max-words))
-        (recur (conj acc nextword))
-        (filter string? acc)))))
+  "Using index m, dir :forward-index or :backward-index, starting at key k, generate sequence of tuples.
+  Stops at a ::boundary, or after max words, whatever comes first."
+  [{:keys [tuple-size] :as idx} dir k max-words]
+  (let [m (get idx dir)]
+    (loop [acc (if (coll? k) k [k])]
+      (let [prefix (subvec acc (max (- (count acc) tuple-size) 0))
+            d (get-in m (encode-prefix idx prefix))
+            nextword (when (pos? (count d))
+                       (gen/weighted d))]
+        (if (and d nextword (< (count acc) max-words))
+          (recur (conj acc nextword))
+          (filter string? acc))))))
 
 (defn generate-forward [idx start-at]
   (let [{:keys [tuple-size forward-index]} idx
         start (or start-at
-                  (apply tuple-vector (repeat tuple-size ::boundary)))]
-    (generate-sequence (:forward-index idx) (:tuple-size idx) start 100)))
+                  (vec (repeat tuple-size ::boundary)))]
+    (generate-sequence idx :forward-index start 100)))
 
 (defn generate-with-seed [idx seed]
   (when-let [starts (get-in idx [:entries seed])]
     (let [start (gen/weighted starts)
-          front-half (generate-sequence (:backward-index idx) (:tuple-size idx) (apply tuple-vector (reverse start)) 50)
-          back-half (generate-sequence (:forward-index idx) (:tuple-size idx) start 50)]
+          front-half (generate-sequence idx :backward-index (vec (reverse start)) 50)
+          back-half (generate-sequence idx :forward-index start 50)]
       (concat (reverse (drop (:tuple-size idx) front-half)) back-half))))
 
 (defn generate-1 [idx {:keys [start-at seed]}]
@@ -134,23 +146,21 @@
 
 (defn add-distance [target-words]
   (fn [rel]
-    (assoc rel
-           :distance (Math/abs (- (* (-> rel :tuples count)
-                                     (-> rel :tuples first count)) target-words)))))
+    (assoc rel :distance (Math/abs (- (-> rel :tuples count) target-words)))))
 
 (defn generate [idx {:keys [start-at
                             seed
                             target-length
                             timeout-ms]
                      :or {timeout-ms 100
-                          target-length 50}}]
+                          target-length 30}}]
   (let [{:keys [tuple-size forward-index]} idx
         gen (if seed
               (partial generate-with-seed idx seed)
               (partial generate-forward idx start-at))
         results (chan 1 (comp (map as-rel)
                               (map (add-distance target-length))))]
-    (produce-while-values results gen {:max 100 :timeout-ms timeout-ms})
+    (produce-while-values results gen {:max (min (max 20 timeout-ms) 200) :timeout-ms timeout-ms})
     (->> (async/<!! (async/into [] results))
          (sort-by :distance)
          first
@@ -159,14 +169,65 @@
 
 (comment
 
-(def idx (atom (markov-index-factory 2)))
+  (def idx (atom (markov-index-factory 2)))
 
-(with-open [rdr (io/reader (io/resource "erowid.txt"))]
-  (doseq [e (line-seq rdr)]
-    (swap! idx index e)))
+  (with-open [rdr (io/reader (io/resource "erowid.txt"))]
+    (doseq [e (line-seq rdr)]
+      (swap! idx index e)))
 
-(take 10 (keys (:forward-index @idx)))
+  (take 10 (keys (:forward-index @idx)))
 
-(generate @idx {:seed "email"})
+  (generate @idx {:seed "email"})
 
-)
+
+  (require '[criterium.core :as crit])
+
+  (def test-lines (vec (line-seq (io/reader (io/resource "test.txt")))))
+
+  ;; 951 ms
+  (crit/bench
+   (let [idx (atom (->MarkovIndex 2 {} {} {} (default-encoder)))]
+     (doseq [e test-lines]
+       (swap! idx index e))))
+
+  ;; 800 ms
+  (crit/bench
+   (let [idx (atom (->MarkovIndex 2 {} {} {} (nested-encoder)))]
+     (doseq [e test-lines]
+       (swap! idx index e))))
+
+  ;; 846 ms
+  (crit/bench
+   (let [idx (atom (->MarkovIndex 2 {} {} {} (string-encoder ":")))]
+     (doseq [e test-lines]
+       (swap! idx index e))))
+
+  (require '[clj-memory-meter.core :as cmm])
+  (do
+    (def default-idx (atom (->MarkovIndex 2 {} {} {} (default-encoder))))
+    (def nested-idx (atom (->MarkovIndex 2 {} {} {} (nested-encoder))))
+    (def string-idx (atom (->MarkovIndex 2 {} {} {} (string-encoder ":"))))
+    (with-open [rdr (io/reader (io/resource "plotbot/plots.txt"))]
+      (doseq [e (line-seq rdr)]
+        (swap! default-idx index e)
+        (swap! nested-idx index e)
+        (swap! string-idx index e)))
+    {:default (cmm/measure @default-idx)
+     :nested (cmm/measure @nested-idx)
+     :string (cmm/measure @string-idx)})
+
+  (crit/bench
+   (generate-with-seed @default-idx "vicious"))
+
+  ;; string 1.91 ms
+  ;; nested 1.96 ms
+
+  (-> default-idx deref :entries first)
+
+  ;; plots.txt
+  ;; => {:default "392.0 MB", :nested "272.8 MB", :string "391.3 MB"}
+
+  ;; test.txt
+  ;; => {:default "41.8 MB", :nested "29.0 MB", :string "41.4 MB"}
+
+  )
