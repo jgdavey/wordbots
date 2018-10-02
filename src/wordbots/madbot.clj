e176cfda1274fb3c05d82702f2f5e6f2e6fdf918
Joshua Davey <josh@suiteness.com>
2018-10-02 17:08:34 -0400

    Improve madbot: use actual madlib templates

--- src/wordbots/madbot.clj
+++ src/wordbots/madbot.clj
@@ -2,82 +2,87 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]
             [wordbots.protocols :as p]
-            [com.joshuadavey.vecset :as v :refer [vecset]]
+            [wordbots.template :as template]
             [wordbots.util :refer [lines paragraphs]]))
-
-(declare index-text)
-
-(defrecord Token [word part]
-  Object
-  (toString [_] word))
 
 (def index (atom {:adjective nil}))
 
 (def mappings
-  {:adjective  "madbot/adjectives.txt"
-   :action     "madbot/actions.txt"
-   :object     "madbot/objects.txt"
+  {:transitive-action "madbot/actions_transitive.txt"
+   :intransitive-action "madbot/actions_intransitive.txt"
+   :adjective  "madbot/adjectives.txt"
    :animal     "madbot/animals.txt"
+   :number     "madbot/numbers.txt"
    :body-part  "madbot/body_parts.txt"
    :comparator "madbot/comparators.txt"})
 
-(defn categorize [w]
-  (let [word (first (re-seq #"\w+" w))]
-    (some (fn [[part words]]
-            (when (words word) part))
-          @index)))
-
-(defn tokenize [word]
-  (->Token word (categorize word)))
-
-(defn index-text [text]
-  (let [words (str/split text #"\b")]
-    (mapv tokenize words)))
-
-(defn possibly-replace [token]
-  (let [idx (get @index (:part token))]
-    (if (and idx (> (rand) 0.2))
-      (rand-nth idx)
-      token)))
-
-(defn madlib [tokens]
-  (loop [t tokens
-         i 5]
-    (when (zero? i)
-      (println (str/join t)))
-    (if (and (not (zero? i)) (= t tokens))
-      (recur (map possibly-replace tokens) (dec i))
-      (str/join t))))
-
 (defn lines-from-resource [filename]
-  (->> (io/resource filename) slurp lines))
+  (with-open [rdr (-> filename io/resource io/reader)]
+    (into [] (line-seq rdr))))
 
 (defn init* []
   (doseq [[part file] mappings]
     (let [w (lines-from-resource file)]
-      (swap! index assoc part (vecset w)))))
+      (swap! index assoc part w))))
 
-(defn generate* [proverbs]
-  (madlib (index-text (rand-nth proverbs))))
+(defn modify-dispatch [text modifier]
+  (some-> modifier
+          :modifier/name
+          keyword))
+
+(defmulti modify modify-dispatch :default ::pass-through)
+
+(defmethod modify ::pass-through [text _]
+  text)
+
+(defn capitalize [^String text]
+  (str
+   (.toUpperCase (subs text 0 1))
+   (subs text 1)))
+
+(defn pluralize [^String text]
+  (if (.endsWith text "s")
+    text
+    (str text "s")))
+
+(defmethod modify :capitalize [text _]
+  (capitalize text))
+
+(defmethod modify :plural [text _]
+  (pluralize text))
+
+(defn replace? []
+  (> (rand) 0.1))
+
+(defn maybe-random-entry [part-of-speech]
+  (when (replace?)
+    (when-let [idx (get @index (keyword part-of-speech))]
+      (rand-nth idx))))
+
+(defn extract-default [args]
+  (some #(when (= 'default (:modifier/name %))
+           (first (:modifier/args %)))
+        args))
+
+(defn madlib-word [sym & args]
+  (if-let [replacement (maybe-random-entry sym)]
+    (reduce modify replacement args)
+    (extract-default args)))
+
+(defn generate* [proverb-template]
+  (template/evaluate proverb-template madlib-word))
 
 (defrecord Madbot [proverbs]
   p/Bot
   (init [_] (init*))
-  (generate [_ req] (generate* proverbs)))
+  (generate [_ req] (generate* (or (get-in req [:params :proverb])
+                                   (rand-nth proverbs)))))
 
 (defn bot []
   (->Madbot
-   (lines-from-resource "madbot/proverbs.txt")))
+   (lines-from-resource "madbot/parsed.txt")))
 
 (comment
-
-  (def mb (->Madbot ["The apple doesn't fall far from the tree."]))
-  (def mb (bot))
-  (p/init mb)
-  (p/generate mb {})
-
-  (init*)
-  (generate* ["Nothing is certain but death and taxes."
-              "Rome wasn't built in a day."])
-
-  )
+  (def b (bot))
+  (p/init b)
+  (p/generate b))
